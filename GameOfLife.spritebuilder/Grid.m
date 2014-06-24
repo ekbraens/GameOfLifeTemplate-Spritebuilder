47b4cbe6285b465094db1767682a4d6e0c15de00
New <new@News-MacBook-Air.local>
2014-06-24 12:42:44 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -154,7 +154,7 @@ static const int GRID_COLUMNS = 10;
             if (currentCreature.livingNeighbors == 3)
             {
                 currentCreature.isAlive = TRUE;
-                numAlive += 1;
+                numAlive++;
             }
             else if ( (currentCreature.livingNeighbors <= 1) || (currentCreature.livingNeighbors >= 4) )
             {
